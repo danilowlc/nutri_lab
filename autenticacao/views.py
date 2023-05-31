c54431dd82a2287526b2b9a800a7111fd3008a5b
danilowlc <danilowlc@gmail.com>
2023-05-31 17:52:31 -0300

    criando vailidação da senha

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from .utils import password_is_valid
+from django.contrib.auth.models import User
 
 # Create your views here.
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+        email = request.POST.get('email')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not password_is_valid(request, senha, confirmar_senha):
+            return redirect('/auth/cadastro')
+        
+        try:
+            user = User.objects.create_user(username=username, email=email, password=senha, is_active=False)
+            user.save()
+            return redirect('/auth/logar')
+        except:
+            return redirect('/auth/cadastro')
+
 
 def logar(response):
     return HttpResponse("Voce esta na pagina de login")
